3da451fde45112aa05d9bac9b816c2f3c50ccdb1
kareem1942993 <karem1941999@gmail.com>
2024-05-22 01:41:20 +0300

    new

--- my-project/config/server.js
+++ my-project/config/server.js
@@ -1,10 +1,13 @@
+const port = process.env.PORT || 4000;
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: port,
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
+
+// env.int("PORT", 1337),
