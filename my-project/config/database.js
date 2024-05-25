716e1b1abbb8cb0d01962ab7d72fb58c347dd4ae
kareem1942993 <karem1941999@gmail.com>
2024-05-25 16:25:15 +0300

    up

--- my-project/config/database.js
+++ my-project/config/database.js
@@ -6,12 +6,12 @@ module.exports = ({ env }) => {
     mysql: {
       connection: {
         connectionString: env("DATABASE_URL"),
-        host: env("DATABASE_HOST"),
+        host: env("DATABASE_HOST",'localhost'),
         port: env.int("DATABASE_PORT", 3306),
-        database: env("DATABASE_NAME"),
-        user: env("DATABASE_USERNAME"),
-        password: env("DATABASE_PASSWORD"),
-        ssl: env.bool("DATABASE_SSL") && {
+        database: env("DATABASE_NAME",'strapi'),
+        user: env("DATABASE_USERNAME",'strapi'),
+        password: env("DATABASE_PASSWORD",'strapi'),
+        ssl: env.bool("DATABASE_SSL",false) && {
           key: env("DATABASE_SSL_KEY", undefined),
           cert: env("DATABASE_SSL_CERT", undefined),
           ca: env("DATABASE_SSL_CA", undefined),
